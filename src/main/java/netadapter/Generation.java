f638065d9e5dd10f81609970d25dd19eec7fb02c
lpapailiou <lena@kaiki.ch>
2020-10-01 20:34:29 +0200

    test generations with thread pool

--- src/main/java/netadapter/Generation.java
+++ src/main/java/netadapter/Generation.java
@@ -6,27 +6,42 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class Generation {
 
-    int populationSize;
-    HashMap<BoardAdapter, Integer> generations = new HashMap<>();
+    private int populationSize;
+    private final static int THREADPOOL = 200;
+    private HashMap<BoardAdapter, Integer> generations = new HashMap<>();
 
     public Generation(int populationSize) {
         this.populationSize = populationSize;
     }
 
     public NeuralNetwork run(NeuralNetwork net) {
+        ExecutorService executor = Executors.newFixedThreadPool(THREADPOOL);
+
         for (int i = 0; i < populationSize; i++) {
-            BoardAdapter adapter = new BoardAdapter(i == 0 ? net : net.clone());
+            Runnable worker = new BackgroundGame(i == 0 ? net : net.clone(), generations);
+            executor.execute(worker);
+
+
+            /*BoardAdapter adapter = new BoardAdapter(i == 0 ? net : net.clone());
             boolean running = true;
             while (running) {
                 running = adapter.moveSnake();
             }
-            generations.put(adapter, adapter.getFitness());
+            generations.put(adapter, adapter.getFitness());*/
         }
-
+        executor.shutdown();
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("executor service interrupted unexpectedly!");
+        }
         NeuralNetwork best = getBest(generations);
         if (generations.isEmpty()) {
             return best;
@@ -43,5 +58,24 @@ public class Generation {
             map.remove(ad.get(0));
         }
         return ad.isEmpty() ? null : ad.get(0).getNet();
+    }
+
+    static class BackgroundGame implements Runnable {
+        NeuralNetwork net;
+        HashMap<BoardAdapter, Integer> gen;
+        BackgroundGame(NeuralNetwork net, HashMap<BoardAdapter, Integer> generation) {
+            this.net = net;
+            this.gen = generation;
+        }
+
+        @Override
+        public void run() {
+            BoardAdapter adapter = new BoardAdapter(net);
+            boolean running = true;
+            while (running) {
+                running = adapter.moveSnake();
+            }
+            gen.put(adapter, adapter.getFitness());
+        }
     }
 }
